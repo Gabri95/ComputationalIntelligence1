ef1eeced329f8faf4f5556b5a5f4374213f9719b
Gabri95 <gabriele.cesa@gmail.com>
2017-10-31 17:22:43 +0100

    Initial commit

---
+++ torcs-client/my_driver.py
@@ -0,0 +1,27 @@
+from .pytocl.driver import Driver
+from .pytocl.car import State, Command
+
+
+class MyDriver(Driver):
+    
+    def drive(self, carstate: State) -> Command:
+        """
+        Produces driving command in response to newly received car state.
+
+        This is a dummy driving routine, very dumb and not really considering a
+        lot of inputs. But it will get the car (if not disturbed by other
+        drivers) successfully driven along the race track.
+        """
+        command = Command()
+        self.steer(carstate, 0.0, command)
+
+        # ACC_LATERAL_MAX = 6400 * 5
+        # v_x = min(80, math.sqrt(ACC_LATERAL_MAX / abs(command.steering)))
+        v_x = 80
+
+        self.accelerate(carstate, v_x, command)
+
+        if self.data_logger:
+            self.data_logger.log(carstate, command)
+
+        return command
